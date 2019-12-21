441cb467eb68aa2fe1e5b92005255b868330bee6
Ephraimdev <aigbefoephraim@yahoo.com>
2019-12-21 12:54:53 +0100

    set up breact redux files

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import './App.css';
+
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store} >
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <PostForm />
+        <Posts />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
